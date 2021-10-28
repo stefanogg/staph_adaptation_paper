018f71c1c9fa16d0db2869d1167c9553cb269fb1
stefanogg <stefano.giulieri@unimelb.edu.au>
2021-10-28 15:20:20 +1300

    Add files via upload

---
+++ variants_annotation/blast2bed.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+INPUT="$1"
+
+#check if input file is provided
+if [ -f "$INPUT" ];
+then
+    OUTPUT=$(echo $INPUT | sed -e s/.tab$//)'.bed'
+else
+    echo "ERROR: No input file prvided or file does not exist.
+Usage: ./blast2bed <blastoutput.bls>
+The blast file should be in blast outfmt 6 or 7.
+See Readme.org for more details."
+    exit 1
+fi
+
+#converting blast to bed
+echo "converting $INPUT in $OUTPUT"
+
+echo "#This file was generated from $INPUT using blast2bed" > $OUTPUT
+#grep -v '^#' "$INPUT" | awk '{print $2,"\t",$9-1,"\t",$10,"\t",$1}' | sort >> $OUTPUT
+grep -v '^#' "$INPUT" | perl -ane 'if($F[8]<=$F[9]){print join("\t",$F[1],$F[8]-1,$F[9],$F[0],"0","+",$F[8],$F[9]),"\n";}else{print join("\t",$F[1],$F[9]-1,$F[8],$F[0],"0","-", $F[8], $F[9]),"\n";}' | bedtools sort -i stdin >> $OUTPUT
+
+
+echo "done"
