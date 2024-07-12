e420170de27b588cc505bd7a4fc7aad8cf60855e
Stefano Giulieri <stefano.giulieri@unimelb.edu.au>
2024-07-12 16:08:01 +1000

    Add missing scripts
    Add python scripts to process Genbank reference files

---
+++ variants_calling/split-gbk-chrom.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+This is as script to split genbank files into separated sequences
+Here, we only keep the chromosome sequence
+Later we will merge in one script and use an option instead
+'''
+
+import sys
+from Bio import SeqIO
+
+def split_gbk(file):
+    with open(file,'r') as input_handle:
+        seqs=[record for record in SeqIO.parse(input_handle,'genbank')]
+        record=seqs[0]
+        header=record.id
+        with open(record.id+'.gbk','w') as output_handle:
+            SeqIO.write(record, output_handle,'genbank')
+
+def main():
+    if len(sys.argv) == 2:
+        file = sys.argv[1]
+        split_gbk(file)
+    else:
+        print("ERROR: split-gbk.py takes one argument.")
+
+if __name__ == "__main__":
+    main()
+
+        
+        
